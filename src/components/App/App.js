825832780bc8f267bdbc1c6c21c95e2b5dd79ce8
Filip FICO <fitz993m@gmail.com>
2020-11-22 23:14:03 +0100

    1.9. Updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,19 @@
-import "./App.css";
+import { DisplayMapFC } from '../DisplayMap/DisplayMap';
+// import DisplayMap from '../DisplayMap/DisplayMap';
+import Map from '../Map/Map';
+import Vector from '../Vector/Vector';
+import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Map />
+      <h1>----</h1>
+      <Vector />
+      <h1>----</h1>
+      {/* <DisplayMap /> */}
+      <DisplayMapFC />
     </div>
   );
 };
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,5 +1,10 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
+import { MapReducer } from '../components/Map/MapReducer';
+import { VectorReducer } from '../components/Vector/VectorReducer';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  MapReducer,
+  VectorReducer,
+});
 
 export default rootReducer;
