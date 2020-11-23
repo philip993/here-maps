fb591cc32577c68d6955da0ac17be4822a00b8ed
Filip Mihajlovski <fitz993m@gmail.com>
2020-11-23 16:07:20 +0100

    1.16. Updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,11 @@
-import { DisplayMapFC } from '../DisplayMap/DisplayMap';
-// import DisplayMap from '../DisplayMap/DisplayMap';
-import Map from '../Map/Map';
-import Vector from '../Vector/Vector';
-import './App.css';
+import NMap from "../NMap/NMap";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <Map />
-      <h1>----</h1>
-      <Vector />
-      <h1>----</h1>
-      {/* <DisplayMap /> */}
-      <DisplayMapFC />
+      <h1>MAPS</h1>
+      <NMap />
     </div>
   );
 };
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,14 @@
-import { combineReducers } from 'redux';
-import { MapReducer } from '../components/Map/MapReducer';
-import { VectorReducer } from '../components/Vector/VectorReducer';
+import { combineReducers } from "redux";
+import { MapReducer } from "../components/Map/MapReducer";
+import { VectorReducer } from "../components/Vector/VectorReducer";
+import { NMapReducer } from "../components/NMap/NMapReducer";
+import { RoutingReducer } from "../components/Routing/RoutingReducer";
 
 const rootReducer = combineReducers({
   MapReducer,
   VectorReducer,
+  NMapReducer,
+  RoutingReducer,
 });
 
 export default rootReducer;
